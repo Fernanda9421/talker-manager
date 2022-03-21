ad282fa289f6bfc049d1258479173c68ee07c416
Fernanda9421 <fer.candrade21@gmail.com>
2022-03-21 20:19:55 -0300

    refactor exercise 4

--- middlewares/createTalker.js
+++ middlewares/createTalker.js
@@ -1,32 +1,15 @@
 const fsUtils = require('../fs-utils/fs-utils');
 
-let lastId = 4;
-
-function newTalker(id, name, age, talk) {
-  return [
-    {
-      id,
-      name,
-      age,
-      talk,
-    },
-  ];
-}
-
 module.exports = async (req, res) => {
-  const { name, age, talk } = req.body;
+  const { body } = req;
   const talkers = await fsUtils.getTalkers();
-  lastId += 1;
-  await fsUtils.addTalker(newTalker(lastId, name, age, talk));
-  talkers.push({ lastId, name, age, talk });
 
-  res.status(201).json({
-    id: lastId,
-    name,
-    age,
-    talk: {
-      watchedAt: talk.watchedAt,
-      rate: talk.rate,
-    },
-  });
+  const newTalker = {
+    ...body,
+    id: talkers.length + 1,
+  };
+  const allTalkers = [...talkers, newTalker];
+  await fsUtils.addTalker(allTalkers);
+
+  res.status(201).json(newTalker);
 };
